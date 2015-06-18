487607033165f6d79c5a517cb7040596d60ff0a7
Zaphodious <achythlook@gmail.com>
2015-06-18 17:30:23 -0300

    Futzing around with generation
    I can now do both (limited) procedural and (not so limited) schematic
    generation. Yay!

--- java/io/github/zaphodious/essentialsorcery/item/Trowel.java
+++ java/io/github/zaphodious/essentialsorcery/item/Trowel.java
@@ -1,6 +1,12 @@
 package io.github.zaphodious.essentialsorcery.item;
 
+import io.github.zaphodious.essentialsorcery.gen.GenTest;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
 public class Trowel extends ItemSpade {
 
@@ -10,4 +16,20 @@ public class Trowel extends ItemSpade {
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see net.minecraft.item.Item#onItemUse(net.minecraft.item.ItemStack, net.minecraft.entity.player.EntityPlayer, net.minecraft.world.World, net.minecraft.util.BlockPos, net.minecraft.util.EnumFacing, float, float, float)
+	 */
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer playerIn,
+			World worldIn, BlockPos pos, EnumFacing side, float hitX,
+			float hitY, float hitZ) {
+		// TODO Auto-generated method stub
+		//GenTest.makeASimpleBuilding(stack, playerIn, worldIn, pos, side, hitX, hitY, hitZ, 22, 33, 44);
+		GenTest.genTestStructure(stack, playerIn, worldIn, pos, side, hitX, hitY, hitZ);
+		return super.onItemUse(stack, playerIn, worldIn, pos, side, hitX, hitY, hitZ);
+		
+	}
+
+	
+	
 }
